793320fec4b91f41c812a96cd88c23519aacd338
German Vassiljev <93324304+GermanVassiljev@users.noreply.github.com>
2022-01-18 13:35:14 +0200

    Update Program.cs

--- EpicBattle2/Program.cs
+++ EpicBattle2/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.IO;
 
 namespace EpicBattle2
@@ -11,14 +11,33 @@ namespace EpicBattle2
             string[] heroes = GetDataFromFile(rootPath + "heroes.txt");
             string[] Enemies = GetDataFromFile(rootPath + "enemies.txt");
             string[] Weapon = GetDataFromFile(rootPath + "weapon.txt");
+            string[] Armor = GetDataFromFile(rootPath + "armor.txt");
             string RandomHero = GetRandomCharacter(heroes);
             string RandomEnemy = GetRandomCharacter(Enemies);
             string HeroWeapon = GetWeapon(Weapon);
             string EnemyWeapon = GetWeapon(Weapon);
-            Console.WriteLine($"Your hero is {RandomHero}");
+            string HeroArmor = GetRandomCharacter(Armor);
+            string EnemyArmor = GetRandomCharacter(Armor);
+            int HeroHP = GenerateHP(HeroArmor);
+            int EnemyHP = GenerateHP(EnemyArmor);
+
+
+            Console.WriteLine($"Your hero is {RandomHero} in {HeroArmor} {HeroHP} HP");
             Console.WriteLine($"He have {HeroWeapon}");
-            Console.WriteLine($"Your enemy is {RandomEnemy}");
+            Console.WriteLine($"Your enemy is {RandomEnemy} in {EnemyArmor} {EnemyHP} HP");
             Console.WriteLine($"He have {EnemyWeapon}");
+            while (HeroHP >= 0 && EnemyHP >= 0)
+            {
+                HeroHP = HeroHP - Hit(RandomEnemy, EnemyWeapon);
+                EnemyHP = EnemyHP - Hit(RandomHero, HeroWeapon);
+            }
+            if(HeroHP>EnemyHP)
+            {
+                Console.WriteLine($"{RandomHero} Save the day!");
+            }else if(HeroHP<EnemyHP)
+            {
+                Console.WriteLine($"{RandomEnemy} Destroy the world!");
+            }
         }
         public static string GetRandomCharacter(string[] someArray)
         {
@@ -40,5 +59,25 @@ namespace EpicBattle2
             return dataFromFile;
         }
 
+        public static int GenerateHP(string armor)
+        {
+            return armor.Length;
+        }
+
+        public static int Hit(string character ,string weapon)
+        {
+            Random rnd = new Random();
+            int strike = rnd.Next(weapon.Length - 2);
+            Console.WriteLine($"{character} hit {strike}.");
+            if(strike == 0)
+            {
+                Console.WriteLine($"Bad luck. {character} missed the target.");
+            } else if(strike == weapon.Length -2)
+            {
+                Console.WriteLine($"Awesome! {character} made a critical hit!");
+            }
+            return strike;
+
+        }
     }
 }
